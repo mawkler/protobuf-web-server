2b7292966f1b4633d6ded57e7f8d23eacc36282b
Adrian Lökk <adrian.lokk@omegapoint.se>
2023-09-06 14:47:12 +0200

    add client code

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("http://127.0.0.1:3000")
+        .await?
+        .text()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
